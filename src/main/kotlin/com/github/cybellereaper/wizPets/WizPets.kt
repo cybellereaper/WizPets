1eb9afe70f6fd667144bf3a460789db93f5621de
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2025-10-01 09:09:26 -0500

    init

---
+++ src/main/kotlin/com/github/cybellereaper/wizPets/WizPets.kt
@@ -0,0 +1,14 @@
+package com.github.cybellereaper.wizPets
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WizPets : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
